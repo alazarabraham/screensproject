4f000571b715427059e85f5f928e4b559c274fcf
alazarabraham <54673208+alazarabraham@users.noreply.github.com>
2020-12-31 09:52:30 -0500

    styling

--- src/app/components/cards/card/card.component.ts
+++ src/app/components/cards/card/card.component.ts
@@ -7,8 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   today = new Date();
+
+
   constructor() { }
 
+
+  returnDate(): string {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date();
+    var day = days[d.getDay()];
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+      min = 0 + min;
+    }
+    var ampm = "am";
+    if (hr > 12) {
+      hr -= 12;
+      ampm = "pm";
+    }
+    var date = d.getDate();
+    var month = months[d.getMonth()];
+    var year = d.getFullYear();
+    let x: string = month + " " + date + "," + " " + year + " " + hr + ":" + min + " " + ampm;
+    return x;
+  }
   ngOnInit(): void {
   }
 
