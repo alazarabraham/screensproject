3cb350fab3b5ba1acb2f134af12490b2047965c8
bstrubeXpanxion <67436970+bstrubeXpanxion@users.noreply.github.com>
2021-01-05 14:17:37 -0500

    Installed Angular Dev Tools

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'ingestflatfile', component: FlatfileComponent },
   { path: 'ingestrdbms', component: RdbmsComponent },
-  { path: 'ingeststreamdata', component: StreamingdataComponent }
+  { path: 'ingeststreamdata', component: StreamingdataComponent },
+  
 
 
 
